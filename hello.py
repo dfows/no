0071f833dd9f7a0a17444f4adefa073206b8624e
Jessica Kwok <jessica@Pluot.local>
2013-10-07 01:58:00 -0400

    asdf

--- hello.py
+++ hello.py
@@ -92,7 +92,8 @@ def logout():
 
 @app.route('/prog')
 def load_prog():
-  projs = "projx" #Project.query.order_by(desc(Project.date))
+  asdf = Project.query.order_by(desc(Project.date))
+  projs = asdf 
   projectsList = "projxlist"#[p.name for p in projs]
   entries = "hello"#Entry.query.order_by(desc(Entry.date))
   return render_template('prog.html', current='prog', projectsList=projectsList, projects=projs, menu=pages, entries=entries)
